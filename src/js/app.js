2e8161dec07890cea9fbf5d668719e6d0c646fbd
Fernando Goicochea <fernandogoicochea123@gmail.com>
2019-06-11 17:14:10 -0500

    add detail treatment view

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,13 @@ $(".container-nav__link").click(function(){
     $(this).next(".megamenu-container").addClass("active");
     $("body").addClass("active");
 })
+
+//programs view
+$(".my-promotions-container__body__item").click(function(){
+    window.location.href = 'detail-promotion.html';
+})
+
+//only promotion
+$(".btn-reservar").click(function(){
+    $(".modal-contact-us").addClass("active");
+})
