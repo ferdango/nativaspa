5854820085142ac7daeb12801b4219547c688bf4
Fernando Goicochea <fernandogoicochea123@gmail.com>
2019-06-13 17:14:55 -0500

    add service megamenu, falta responsive y pulir all

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 $(".container-nav__link").click(function(){
-    $(this).next(".megamenu-container").addClass("active");
-    $("body").addClass("active");
+    $(".megamenu-container").removeClass("active");
+    $("body").removeClass("active");
+    $(this).next(".megamenu-container").toggleClass("active");
+    $("body").toggleClass("active");
 })
 
 //programs view
@@ -22,3 +24,32 @@ $(".treatment-service-card").click(function(){
 $(".our-procedures__slider__item").click(function(){
     window.location.href = 'detail-sub-treatment.html';
 })
+
+const our_procedures_slider = tns({
+    container: '.our-procedures__slider',
+    items: 3,
+    axis: 'horizontal',
+    autoplay: true,
+    mode: "carousel",
+    //gutter: 50,
+    autoWidth: true,
+    slideBy: 1,
+    nav: false,    
+    autoplay: true,
+    speed: 400,
+    mouseDrag: true,
+    lazyload: true,
+    responsive: {
+      640: {
+        edgePadding: 20,
+        gutter: 20,
+        items: 2
+      },
+      700: {
+        gutter: 30
+      },
+      900: {
+        items: 3
+      }
+    }
+});
